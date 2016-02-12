94f15d64b5bc71df6d078e8f24ea8d740bbb819d
hmlab book pro <saki.tommy69@gmail.com>
2016-02-12 16:59:56 +0900

    変更しました

--- gacha2/ViewController.swift
+++ gacha2/ViewController.swift
@@ -8,7 +8,7 @@
 
     import UIKit
     
-    class ResulrviewControllerViewController: UIViewController {
+    class ViewController: UIViewController {
         
         
         override func viewDidLoad() {
